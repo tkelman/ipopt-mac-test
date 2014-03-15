91439168f5215873d891f388749caf089da4b7fb
Tony Kelman <tony@kelman.net>
2014-03-15 01:00:35 -0700

    create build.sh for Ipopt 3.11.4

---
+++ build.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+set -e
+svn co https://projects.coin-or.org/svn/Ipopt/releases/3.11.4 .
+prefix=$PWD/build01
+
+
+cd ThirdParty/Mumps
+./get.Mumps
+cd ../..
+for i in Blas Lapack; do
+  cd ThirdParty/$i
+  ./get.$i
+  mkdir -p build
+  cd build
+  ../configure --disable-shared --enable-static --with-pic --prefix=$prefix
+  make install
+  cd ../../..
+done
+mkdir -p build
+cd build
+../configure --with-blas=$prefix/lib/libcoinblas.a \
+  --with-lapack=$prefix/lib/libcoinlapack.a \
+  --prefix=$prefix --enable-dependency-linking
+make install
+make test
+otool -L $prefix/lib/libipopt.dylib
+ls $prefix/lib
